b1572f9034c12f6ba27ef37ccc13db06a8df316d
hkKevin <pakkiuleung@gmail.com>
2019-03-22 21:38:49 +1100

    Added particles.js  (dynamic animation) on header. Header contents are vertical aligned in narrow screen. Unified the size of skill logos. Removed project (Burger App). Added icon links to my LinkedIn and GitHub profile. Added more animation on scroll.

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ $(document).ready(function () {
     $('#copy-hint').attr('aria-label', 'Copy');
   }
 
+
   // Burger icon onClick
   $('#burger-icon').click(function () {
 
@@ -61,13 +62,99 @@ $(document).ready(function () {
 
   });
 
+
   // Burger icon on hover
   $("#burger-icon").hover( function() {
     $("#bar-1, #bar-2, #bar-3").toggleClass("burger-bar-hover");
   });
+
+
+  // Particles animation
+  particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 20,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#74819a"
+      },
+      "shape": {
+        "type": "circle"
+      },
+      "opacity": {
+        "value": 0.7,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 3,
+        "random": true,
+        "anim": {
+          "enable": true,
+          "speed": 5,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 300,
+        "color": "#74819a",
+        "opacity": 1,
+        "width": 0.3
+      },
+      "move": {
+        "enable": true,
+        "speed": 0.3,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1202.559045649142
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "window",
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode": "bubble"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "resize": true
+      },
+      "modes": {
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 300,
+          "duration": 10
+        }
+      }
+    },
+    "retina_detect": true
+  });
   
 });
-
-// $(window).on('load', function() {
-//   AOS.refresh();
-// });
